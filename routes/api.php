17efb497f955c6a0212c9776b6e58c73c81e2dc0
ansleyantonio <ansley.d@pengroup.com>
2025-10-06 23:30:10 +0100

    added tasks endpoints
    can create and assign a test
    filter tests by status
    Priority filtering for tasks

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,3 +26,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // Route::apiResource('tasks', TaskController::class);
 
 Route::apiResource('projects', ProjectController::class);
+
+Route::apiResource('tasks', TaskController::class);
